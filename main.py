fd8f6c70cb4ddcf21f4df8fd96b9cf031db36a56
Daniel Depaoli <daniel.depaoli@gmail.com>
2016-10-31 08:20:56 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import exifread
+import re
+
+folder_list = ["/Users/daniel/Pictures/DCIM/100ANDRO"]
+
+def main():
+    for folder in folder_list:
+        for file in os.listdir(folder):
+            if file.endswith(".jpg") or file.endswith(".JPG"):
+                f = open(folder + "/" + file, 'rb')
+                tags = exifread.process_file(f)
+                date = re.sub(":", "-", tags["Image DateTime"].printable)
+                print file, "-->", date
+
+if __name__ == '__main__':
+    main()
